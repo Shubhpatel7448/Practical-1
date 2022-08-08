3f3420ee0dd5956b9302f24aef88e5777f399dd3
Shubhpatel7448 <shatel7448@gmail.com>
2022-08-08 10:06:23 +0530

    Practical - 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
 fun main(){
     print("hello world")
+    val integer : Int = 7
+    println("integer : $integer")
+    val float = 1.5F
+    println("float : $float")
+    val character : Char = 'x'
+    println("character : $character")
+    val string : String = "Hi"
+    println("String : $string")
+    val bool : Boolean = true
+    println("Boolean : $bool")
+    val double : Double = 96.36
+    println("Double : $double")
+    val long : Long = 20012011127
+    println("Long : $long")
+    val short : Short = -2
+    println("Short : $short")
+    val byte : Byte = 127
+    println("Byte : $byte")
 }
