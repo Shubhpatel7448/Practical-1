b1eef5d2f9d60cebcc0e4b1564f97423a5bbeb66
Shubhpatel7448 <shatel7448@gmail.com>
2022-08-08 10:51:36 +0530

    Practical - 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,45 @@
-fun main(){
-    print("hello world")
-    val integer : Int = 7
+import java.lang.reflect.Type
+
+fun main() {
+    println("hello world")
+    println("-------------------------Variables--------------------------------------")
+    val integer: Int = 7
     println("integer : $integer")
     val float = 1.5F
     println("float : $float")
-    val character : Char = 'x'
+    val character: Char = 'x'
     println("character : $character")
-    val string : String = "Hi"
+    val string: String = "Hi"
     println("String : $string")
-    val bool : Boolean = true
+    val bool: Boolean = true
     println("Boolean : $bool")
-    val double : Double = 96.36
+    val double: Double = 96.36
     println("Double : $double")
-    val long : Long = 20012011127
+    val long: Long = 20012011127
     println("Long : $long")
-    val short : Short = -2
+    val short: Short = -2
     println("Short : $short")
-    val byte : Byte = 127
+    val byte: Byte = 127
     println("Byte : $byte")
+    p2()
+
+}
+fun p2() {
+
+    println("----------------------------Type Casting--------------------------------")
+
+    val x = 7
+    println("Integer : $x")
+    x.toDouble()
+    println("Int to Double : $x ")
+
+    val string = "20"
+    println("String : $string")
+    string.toInt()
+    println("String to Int : $string")
+
+    string.toDouble()
+    println("String to Double : $string")
+
+
 }
